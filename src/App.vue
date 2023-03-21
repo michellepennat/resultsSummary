e01916b38be85fbcf4e80ad7e107e00e4cbd99e4
Michelle Penna <mp.design.apps@gmail.com>
2023-03-21 15:29:48 -0500

    format

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
   <main>
     <div class="attribution">
       Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      <a
+        href="https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV"
+        target="_blank"
         >Frontend Mentor</a
-      >. Coded by <a href="#">Your Name Here</a>.
+      >. Coded by
+      <a href="https://github.com/michellepennat">Michelle Penna</a>.
     </div>
   </main>
 </template>
