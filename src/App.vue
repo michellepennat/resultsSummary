fbf7c1b9df7e955d5f851d108203aa3dd5580cfd
Michelle Penna <mp.design.apps@gmail.com>
2023-03-21 15:15:34 -0500

    add sass

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <main></main>
+  <main>
+    <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+        >Frontend Mentor</a
+      >. Coded by <a href="#">Your Name Here</a>.
+    </div>
+  </main>
 </template>
 
 <script>
