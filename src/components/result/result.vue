5753bf3acefd8cc9582c1e60633ac3325984270b
Michelle Penna <mp.design.apps@gmail.com>
2023-03-21 15:29:30 -0500

    feat(result): add result

---
+++ src/components/result/result.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <div class="attribution">
+      Challenge by
+      <a
+        href="https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV"
+        target="_blank"
+        >Frontend Mentor</a
+      >. Coded by
+      <a href="https://github.com/michellepennat">Michelle Penna</a>.
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "ResultPage",
+};
+</script>
